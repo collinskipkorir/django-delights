f0badc7e303523bf11de6612f7f24ea78cd108d6
Dominik <dmkk1@outlook.com>
2022-05-12 17:56:28 +0200

    Add models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30, default="")
+    quantity = models.FloatField(default=0.0)
+    unit = models.CharField(max_length=15, default="")
+    unit_price = models.FloatField(default=0.0)
+    
+    def __str__(self):
+        return self.name + ": " + self.quantity + " for " + self.unit_price + " per " + self.unit
+    
+class MenuItem(models.Model):
+    title = models.CharField(max_length=100, default="")
+    price = models.FloatField(default=1.0)
+    
+    def __str__(self):
+        return self.title + " for " + self.price
+    
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(to=MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(to=Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0.0)
+    
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(to=MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=datetime.date.today)
